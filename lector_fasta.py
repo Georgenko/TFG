4fc70cf08adae999d27cd0ffbfbf388d8a5c9398
Georgenko <g.stanimirov.m@gmail.com>
2021-04-07 03:52:46 +0300

    parte 1 casi hecha

--- lector_fasta.py
+++ lector_fasta.py
@@ -1,21 +1,122 @@
+#encoding:utf-8
+
+
+from bytes_leidos import BytesLeidos
+
+
 class LectorFASTA:
     
     
     def __init__(self,filename):
-        self.filename=filename
+        
+        bl=self.leer_fichero(filename)
+        
+        self.contenido=bl.contenido
+        self.bytes_validos=bl.bytes_validos
+        
+        
+    def leer_fichero(self,filename):
+    
+        # f=file
+        with open(filename) as f:
+            
+            contenido=bytearray()
+            # print(contenido)
+            
+            bytes_read=0
+     
+            # l=line
+            for l1 in f:
+                # print(l1)
+                # print(type(l1))
+                
+                l2=l1.rstrip("\n").upper()
+                # print(l2)
+                
+                len_line=len(l2)
+                print(len_line)
+                
+                l3=l2.encode()
+                # print("l3 =",l3)
+                # print("l2[0] =",l2[0],"->","l3[0] =",l3[0])
+                # print("l2[1] =",l2[1],"->","l3[1] =",l3[1])
+                
+                # b=byte
+                for b in l3:
+                    # print(b)
+                    # print(type(b))
+                    
+                    contenido.append(b)
+                    
+                bytes_read+=len_line
+    
+        # print(contenido)
+        # print(bytes_read)
+    
+        # comprobar que se ha cerrado el fichero correctamente con with:
+        # print(f.closed)
+        
+        return BytesLeidos(contenido,bytes_read)
+    
+
+    def comparar(self,patron,posicion):
+        # print("patron:",patron)
+        # print("Longitud de patron:",len(patron))
+        # print("posicion:",posicion)
+        # print("comparar se ha invocado.")
+        
+        for i in range(len(patron)):
+            # print("contenido ? patron en",posicion,i,":\t",self.contenido[posicion+i],"?",patron[i])
+            
+            if patron[i]!=self.contenido[posicion+i] and patron[i]!="N".encode()[0]:
+                return False
+                
+        return True
+    
+    
+    def buscar(self,patron):
+        # print("patron:",patron)
+        # print("Longitud de patron:",len(patron))
+        # print("bytes_validos:",self.bytes_validos)
+        
+        posiciones=[]
+
+        p=patron.upper().encode()
+        
         try:
-            leer_fichero(self.filename)
+            
+            print("Número de comparaciones que se realizarán:",self.bytes_validos-len(patron)+1)
+            for i in range(self.bytes_validos-len(patron)+1):
+                
+                if self.comparar(p,i):
+                    
+                    posiciones.append(i)
+                    
         except:
-            print("fallo leer_fichero")
-        else:
-            print("suceso leer_fichero")
+            
+            print("Algo ha fallado en buscar.")
+        
+        return posiciones
+    
+    
+    def get_secuencia(self,posicion,tamano):
 
+        s=""
+        
+        for i in range(tamano):
+            
+            s+=chr(self.contenido[posicion+i])
+            
+        return s
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
-    #es necesario, o simplemente uso lf.filename?
-    def __str__(self):
-        return f"El fichero seleccionado es {self.filename}"
-
-
-def leer_fichero(filename):
-    print("leer_fichero invocado")
-
+    
--- main.py
+++ main.py
@@ -1,20 +1,47 @@
-#coding=utf-8
+
+
 import sys
+
+
 from lector_fasta import LectorFASTA
 
 
-if(len(sys.argv)==1):
+if len(sys.argv)==1:
+    
     print("No se han proporcionado los parámetros.")
+    
     sys.exit()
-
-
-"""
-if(len(sys.argv)==2):
+    
+elif len(sys.argv)==2:
+    
     print("No se ha proporcionado el patrón.")
+    
     sys.exit()
-"""
-
-
-lf=LectorFASTA(sys.argv[1])
-print(lf)
-#print(lf.filename)
+    
+else:
+    
+    try:
+        
+        lf=LectorFASTA(sys.argv[1])
+    # esto invoca el método __str__ de la clase LectorFasta:
+    # print(lf)
+    # esto te imprime el atributo filename de la instancia lf:
+    # print(lf.filename)
+    
+        print(lf.contenido)
+        
+        #p=patrón
+        p=sys.argv[2]
+        
+        #pos=lista con posiciones
+        pos=lf.buscar(p)
+        lf.get_secuencia(0,2)
+        for i in range(len(pos)):
+            print("Encontrado",p,"en",pos[i],":",lf.get_secuencia(pos[i],len(p)))
+        
+    except OSError as e:
+        print(e)
+        
+        
+        
+        
